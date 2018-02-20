582fbcb906f473fa82078b0856b30e4e99aa5fcf
Ignacio <idx001@gmail.com>
2018-02-20 18:08:00 -0300

    chart again

---
+++ interface/chartRender.js
@@ -0,0 +1,73 @@
+var chartCnf = {
+	responsive: true,
+	hoverMode: 'index',
+	stacked: false,
+	title: {
+		display: true,
+		text: 'Grafico de temperatura'
+	},
+	scales: {
+		yAxes: [{
+				type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+				display: true,
+				position: "left",
+				id: "y-axis-1",
+			},
+			{
+				type: "linear", // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+				display: true,
+				position: "left",
+				id: "y-axis-2",
+			}
+		],
+	}
+};
+var speedData = {
+	labels: [],
+	datasets: [{
+			label: "Temperatura",
+			data: [],
+			borderColor: "#0368AE",
+			lineTension: 0.3,
+			fill: 'start',
+			yAxisID: "y-axis-1",
+		}, {
+			label: "Temperatura a alcanzar",
+			data: [],
+			borderColor: "#26B99A",
+			lineTension: 0.3,
+			fill: 'start',
+			yAxisID: "y-axis-2"
+		}
+	]
+
+};
+function renderMainChart(data) {
+	speedData.labels = data.labels;
+	speedData.datasets[0].data = data.values;
+	speedData.datasets[1].data = data.valuesExp;
+
+	var lineChart = new Chart($("#speedChart"), {
+			type: 'line',
+			data: speedData,
+			options: chartCnf
+		});
+}
+$(document).ready(function () {
+	var interval = setInterval(function () {
+			if ($('#speedChart').length) {
+				$.ajax({
+					type: 'GET',
+					dataType: "json",
+					url: '/getFermGraphData.json?id=' + $('#fermValId').val(),
+					processData: true,
+					async: false,
+					success: function (resp) {
+						renderMainChart(resp);
+					}
+				});
+				clearInterval(interval);
+				//reiniciarlo en algun lado
+			}
+		}, 5000);
+});
