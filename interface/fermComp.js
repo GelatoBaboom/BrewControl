79bbd09147f8396058376cf9e009434ef414cc45
Ignacio <idx001@gmail.com>
2018-02-24 11:52:33 -0300

    arreglado go back en fermentaciones de mas de dos digitos

--- interface/fermComp.js
+++ interface/fermComp.js
@@ -29,8 +29,8 @@ define([], function () {
 		created: function () {
 			this.viewFermentacion(this.$route.params.id);
 			//this.urlback =  '/'+this.$route.params.returl.replace(/\-/i,'/')			;
-			console.log(this.$route.path.replace(/\/ferm\/\d?\//i,'').replace(/\-/i,'/'));
-			this.urlback =  '/'+this.$route.path.replace(/\/ferm\/\d?\//i,'').replace(/\-/i,'/');
+			console.log(this.$route);
+			this.urlback =  '/'+this.$route.path.replace(/\/ferm\/\d*\//i,'').replace(/\-/i,'/');
 		},
 		methods: {
 
